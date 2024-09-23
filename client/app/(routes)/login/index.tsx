6f17b1c2b0204373b47b11e9890a436f648d3ff3
Jordan Defay <jordan.defay@hotmail.com>
2024-09-23 09:51:00 +0100

    Projet elearning React

---
+++ client/app/(routes)/login/index.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from 'react-native'
+import LoginScreen from '@/screens/auth/login/login.screen'
+export default function Login() {
+  return (
+    <LoginScreen />
+  )
+}
---
+++ client/app/(routes)/welcome-intro/index.tsx
@@ -0,0 +1,7 @@
+import WelcomeIntroScreen from "@/screens/welcome/welcome.intro";
+
+export default function WelcomeIntro() {
+  return (
+    <WelcomeIntroScreen />
+  )
+}
